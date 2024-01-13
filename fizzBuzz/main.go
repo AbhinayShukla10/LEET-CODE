2ca1bf026bb1b1f6c8ee8ac1bac2172b663761e1
Abhinay <abhinayshukla10@gmail.com>
2024-01-13 12:17:29 +0530

    fizzbuzz coompleted

--- fizzBuzz/main.go
+++ fizzBuzz/main.go
@@ -1,7 +1,34 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"strconv"
+)
 
 func main() {
-	fmt.Println("hello")
+	f3 := fizzBuzz(100)
+	fmt.Println(f3)
+}
+func fizzBuzz(n int) []string {
+	var s []string
+	for i := 1; i <= n; i++ {
+		s = append(s, find(i))
+
+	}
+	return s
+}
+func find(i int) string {
+	switch {
+	case i%3 == 0 && i%5 == 0:
+		return "FizzBuzz"
+
+	case i%3 == 0:
+		return "Fizz"
+
+	case i%5 == 0:
+		return "Buzz"
+	default:
+		return strconv.Itoa(i)
+	}
+
 }
